4f199d9b8cb6e79d73d53819ba93cc10a17cff59
DanielT2828 <144031363+DanielT2828@users.noreply.github.com>
2024-03-14 13:55:24 +0100

    DB Änderung

--- Ordner/app/db-init/init-db.sql
+++ Ordner/app/db-init/init-db.sql
@@ -7,8 +7,22 @@ CREATE TABLE IF NOT EXISTS users (
   password VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE messages (
+    message_id INT AUTO_INCREMENT PRIMARY KEY,
+    sender_id INT NOT NULL,
+    receiver_id INT NOT NULL,
+    text VARCHAR(255) NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (sender_id) REFERENCES users(user_id),
+    FOREIGN KEY (receiver_id) REFERENCES users(user_id)
+);
+
+
+
 -- Optional: Füge einen Testbenutzer hinzu
 INSERT INTO users (username, password) VALUES ('testuser', 'testpass');
+INSERT INTO users (username, password) VALUES ('admin', '$2a$10$RE/PCtpafR/l8LMDmYTnQeqh5X/RempVo.TGDXTSIl99aPphh24Nm');
+
 
 -- Gewährt dem Benutzer exampleuser alle Rechte auf die Datenbank Benutzerdatenbank
 GRANT ALL PRIVILEGES ON Benutzerdatenbank.* TO 'exampleuser'@'%' IDENTIFIED BY 'examplepass';
